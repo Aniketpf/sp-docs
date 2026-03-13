4f8236eb432a05cc5bffdbb2c9e61d45e4029487
Aniketpf <aniket.saha@prismforce.ai>
2026-03-13 18:04:03 +0530

    fix: updated the docusaurus config for algolia search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,162 +5,163 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-    title: 'SelectPrism',
-    tagline: 'SelectPrism API Documentation',
-    favicon: 'https://sp-bsl.pfsit.xyz/integration/apple-touch-icon.png',
+  title: 'SelectPrism',
+  tagline: 'SelectPrism API Documentation',
+  favicon: 'https://sp-bsl.pfsit.xyz/integration/apple-touch-icon.png',
 
-    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-    future: {
-        v4: true, // Improve compatibility with the upcoming Docusaurus v4
-    },
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
 
-    // Set the production url of your site here
-    url: 'http://localhost:3000',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+  // Set the production url of your site here
+  url: process.env.PRODUCTION_URL ?? 'http://localhost:3000',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
-    onBrokenLinks: 'throw',
+  onBrokenLinks: 'throw',
 
-    // Even if you don't use internationalization, you can use this field to set
-    // useful metadata like html lang. For example, if your site is Chinese, you
-    // may want to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
 
-    plugins: [
-        './plugins/webpack-plugin.js',
-        [
-            'docusaurus-plugin-openapi-docs',
-            {
-                id: 'openapi',
-                docsPluginId: 'classic',
-                config: {
-                    api: {
-                        specPath: 'openapi/api-spec.json',
-                        outputDir: 'docs/api',
-                        sidebarOptions: {
-                            groupPathsBy: 'tag',
-                        },
-                    },
-                },
+  plugins: [
+    './plugins/webpack-plugin.js',
+    [
+      'docusaurus-plugin-openapi-docs',
+      {
+        id: 'openapi',
+        docsPluginId: 'classic',
+        config: {
+          api: {
+            specPath: 'openapi/api-spec.json',
+            outputDir: 'docs/api',
+            sidebarOptions: {
+              groupPathsBy: 'tag',
             },
-        ],
+          },
+        },
+      },
     ],
+  ],
 
-    themes: ['docusaurus-theme-openapi-docs'],
+  themes: ['docusaurus-theme-openapi-docs'],
 
-    presets: [
-        [
-            'classic',
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          docItemComponent: '@theme/ApiItem',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    algolia: {
+      appId: process.env.APPLICATION_ID ?? 'test',
+      apiKey: process.env.ALGOLIA_SEARCH_API_KEY ?? 'test',
+      indexName: 'my-docs-local',
+      contextualSearch: false,
+      searchPagePath: 'search',
+    },
+    navbar: {
+      title: 'SelectPrism',
+      logo: {
+        alt: 'SelectPrism Logo',
+        src: 'https://sp-bsl.pfsit.xyz/integration/apple-touch-icon.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'apisidebar',
+          position: 'left',
+          label: 'API',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'webhookssidebar',
+          position: 'left',
+          label: 'Webhooks',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
             {
-                docs: {
-                    sidebarPath: './sidebars.ts',
-                    docItemComponent: '@theme/ApiItem',
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                blog: false,
-                theme: {
-                    customCss: './src/css/custom.css',
-                },
-            } satisfies Preset.Options,
-        ],
-    ],
-
-    themeConfig: {
-        // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
-        colorMode: {
-            respectPrefersColorScheme: true,
-        },
-        algolia: {
-            appId: '4NFQAL2J5A',
-            apiKey: 'b64f9f2b583cf9cdcfed211e24b0275b',
-            indexName: 'my-docs-local',
-            contextualSearch: false,
-            searchPagePath: 'search',
-        },
-        navbar: {
-            title: 'SelectPrism',
-            logo: {
-                alt: 'SelectPrism Logo',
-                src: 'https://sp-bsl.pfsit.xyz/integration/apple-touch-icon.png',
+              label: 'Tutorial',
+              to: '/docs/intro',
             },
-            items: [
-                {
-                    type: 'docSidebar',
-                    sidebarId: 'apisidebar',
-                    position: 'left',
-                    label: 'API',
-                },
-                {
-                    type: 'docSidebar',
-                    sidebarId: 'webhookssidebar',
-                    position: 'left',
-                    label: 'Webhooks',
-                },
-                {
-                    href: 'https://github.com/facebook/docusaurus',
-                    label: 'GitHub',
-                    position: 'right',
-                },
-            ],
+          ],
         },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: 'Docs',
-                    items: [
-                        {
-                            label: 'Tutorial',
-                            to: '/docs/intro',
-                        },
-                    ],
-                },
-                {
-                    title: 'Community',
-                    items: [
-                        {
-                            label: 'Stack Overflow',
-                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discordapp.com/invite/docusaurus',
-                        },
-                        {
-                            label: 'X',
-                            href: 'https://x.com/docusaurus',
-                        },
-                    ],
-                },
-                {
-                    title: 'More',
-                    items: [
-                        {
-                            label: 'GitHub',
-                            href: 'https://github.com/facebook/docusaurus',
-                        },
-                    ],
-                },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/docusaurus',
+            },
+          ],
         },
-        prism: {
-            theme: prismThemes.github,
-            darkTheme: prismThemes.dracula,
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
         },
-    } satisfies Preset.ThemeConfig,
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 };
 
 export default config;
